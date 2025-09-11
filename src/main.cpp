d7365fe7a7d6cbcdcce357b8943db37a042ea407
Nathan R <nhrussell06@gmail.com>
2025-09-11 16:53:45 -0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <CAN.h>
+
+constexpr int CAN_RX = 20;
+constexpr int CAN_TX = 21;
+constexpr int POT= 26;
+constexpr int MOTOR_FORWARDS = 5;
+constexpr int MOTOR_BACKWARDS = 4;
+
+void setup() {
+
+}
+
+void loop() {
+    
+}
