4ab10a198750dc5ae1aac51262bd28cd79966411
Nathan R <nhrussell06@gmail.com>
2025-10-06 13:58:51 -0700

    read front and rear pressure

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <CAN.h>
 #include <map>
 #include <EEPROM.h>
+#include <r3.h>
 
 struct SavedData {
     uint32_t signature;
@@ -20,10 +21,9 @@ constexpr uint32_t SIGNATURE = 0xB1A5B1A5;
 constexpr int EEPROM_SIZE = sizeof(savedData);
 constexpr int EEPROM_ADDRESS = 0;
 
+SavedData savedData;
 
 int potValue;
-
-SavedData savedData;
 
 //TODO: add CRC and front-end indicator of success or failure
 void loadsavedData() {
@@ -38,21 +38,66 @@ void loadsavedData() {
 //TODO: add CRC
 void savePosition() {
     savedData.signature = SIGNATURE;
-    savedData.position = potValue;
+    savedData.position = readPotValue();
     //savedData.crc = 
 
     EEPROM.put(EEPROM_ADDRESS, savedData);
 };
 
+
 int readPotValue() {
     return analogRead(POT);
 };
 
-int readFrontBrakePressure() {
+
+
+/* 
+ * for brake pressure: map all values (pairing front and back) over a period of time 
+ * and then select the highest value pair, since the ratio is most consistent when max
+ * pressure is applied
+ */
+
+//0x36B 20hz TX 0-1 Unsigned kPa y = x - 101.3
+double readFrontBrakePressure() {
+    int packetSize = CAN.parsePacket();
+    if (packetSize == R3_GROUP08_LENGTH && CAN.packetId() == R3_GROUP08_FRAME_ID && !CAN.packetExtended()) {
+        uint8_t buf[8];
+        for (int i = 0; i < 8; i++) buf[i] = CAN.read;
+
+        r3_group08_t message;
+        if (r3_group08_unpack(message, buf, 8) == 0) {
+            double kpa = r3_group08_brake_pressure_front_decode(message.brake_pressure_front);
+            return kpa;
+        };
+    };
+    return -1.0;
+};
+
+//0x476 20hz TX 0-1 Unsigned kPa y = x-101.3
+double readRearBrakePressure() {
+    int packetSize = CAN.parsePacket();
+    if (packetSize == R3_GROUP45_LENGTH && CAN.packetId == R3_GROUP45_FRAME_ID && !CAN.packetExtended) {
+        uint8_t buf[8];
+        for (int i = 0, i < 8, i++) buf[i] = CAN.read();
+
+        r3_group45_t message;
+        if (r3_group45_unpack(message, buf, 8) == 0) {
+            double kpa = r3_group45_brake_pressure_rear_decode(message);
+            return kpa;
+        };
+    };
+    return -1.0;
+};
+
+std::map mapPressures() {
+
+}
+
+int findMaxValues() {
 
 };
 
-int readRearBrakePressure() {
+double calculateRatio() {
 
 };
 
